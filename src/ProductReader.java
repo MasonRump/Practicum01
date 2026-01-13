d1c783efa1de49535bc2773414d7e4ddc8babe05
Mason Rump <Mason.rump@yahoo.com>
2026-01-13 17:22:34 -0500

    Commit 5: Added an import

--- src/ProductReader.java
+++ src/ProductReader.java
@@ -1,6 +1,7 @@
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
