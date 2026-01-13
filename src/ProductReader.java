81500bd49f25f3e7f8270c2e9c7e6e0ebffb3a7a
Mason Rump <Mason.rump@yahoo.com>
2026-01-13 17:21:09 -0500

    Commit 4 Created and Completed Product Reader

---
+++ src/ProductReader.java
@@ -0,0 +1,45 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import javax.swing.JFileChooser;
+
+public class ProductReader {
+    static void main(String[] args) {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new java.io.File(System.getProperty("user.dir")));
+
+        if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            Path file = chooser.getSelectedFile().toPath();
+            ArrayList<String> lines = new ArrayList<>();
+
+            try (InputStream in = new BufferedInputStream(Files.newInputStream(file));
+                 BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+
+                while (reader.ready()) {
+                    lines.add(reader.readLine());
+                }
+
+                System.out.printf("%-8s %-15s %-30s %-10s%n", "ID#", "Name", "Description", "Cost");
+                System.out.println("===============================================================");
+
+                for (String line : lines) {
+                    String[] f = line.split(",");
+                    if (f.length == 4) {
+                        System.out.printf("%-8s %-15s %-30s $%-10s%n"
+                                , f[0].trim(), f[1].trim(), f[2].trim(), f[3].trim());
+                    } else {
+                        System.out.println("Error record: " + line);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            System.out.println("No file selected");
+        }
+    }
+}
